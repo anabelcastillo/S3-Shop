f212dbf18071e49750b0066e86b310bb3dc855c5
anabelcastillo <anabelcas@gmail.com>
2023-02-02 12:10:49 +0100

    "Exercici 1"

--- js/shop.js
+++ js/shop.js
@@ -73,6 +73,8 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
+    const productFind = products.find((product) => product.id === id);
+    cartList.push(productFind)
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
 }
